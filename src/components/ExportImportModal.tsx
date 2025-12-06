9f2df8b689e7b8b4b72e033ae1a9084c3d3065a4
Miodec <jack@monkeytype.com>
2025-12-06 14:05:35 +0100

    fix: remove margin from modal container for better alignment

--- src/components/ExportImportModal.tsx
+++ src/components/ExportImportModal.tsx
@@ -86,7 +86,7 @@ export function ExportImportModal({
 
   return (
     <div 
-      className="fixed inset-0 bg-black/50 flex items-center justify-center z-50"
+      className="fixed inset-0 bg-black/50 flex items-center justify-center z-50 m-0"
       onClick={onClose}
     >
       <div 
